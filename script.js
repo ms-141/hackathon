71854f9d51abcb84eb0509962515ac119873fba1
Muji <128879165+ms-141@users.noreply.github.com>
2025-10-04 13:09:55 -0600

    added functional forms

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const root = document.getElementsByClassName("root")[0];
 
 const newButton = document.createElement("button");
@@ -13,3 +12,46 @@ btn.onclick = () => {
     heading.textContent = "TA-DA!";
     root.appendChild(heading);
 };
+
+
+//get info from the form
+document.getElementById('userForm').addEventListener('submit', function (event) {
+    event.preventDefault(); // prevent default form submission
+
+    const form = new FormData(event.target);
+    const name = form.get('name');
+    const date = form.get('date');
+    const entry = form.get('entry');
+
+    console.log("Name:", name);
+    console.log("Date:", date);
+    console.log("Entry:", entry);
+});
+
+//image preview logic
+document.getElementById('imageUploadForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const form = new FormData(event.target);
+
+
+    const imageFile = form.get('myImage');
+
+    // Check if a file was actually selected
+    if (!imageFile || imageFile.size === 0) {
+        console.log('No file selected');
+        return;
+    }
+
+    const tempURL = URL.createObjectURL(imageFile);
+
+    const imagePreview = document.getElementById('imagePreview')
+    imagePreview.src = tempURL;
+    imagePreview.style.display = 'block';
+
+    //remove the temporary URL to free up memory
+    imagePreview.onload = () => {
+        URL.revokeObjectURL(tempURL);
+    };
+
+})
+
